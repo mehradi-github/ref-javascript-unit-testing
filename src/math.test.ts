2b216e261872132ca20f1fd263e9152f23e73492
Mehradi <mehradi.info@gmail.com>
2023-11-25 19:09:38 +0100

    Adding test 'Should transform a string number to a number of type number'

--- src/math.test.ts
+++ src/math.test.ts
@@ -2,10 +2,20 @@ import { expect, it } from "vitest";
 import { Sum } from "./math";
 
 it("Should yield the expected sum of numbers", async () => {
+  //Arrange
   const numbers: number[] = [1, 3];
   const expectedResult = 4;
-
+  //Act
   const result = Sum(numbers);
-
+  //Assert
   expect(result).toEqual(expectedResult);
+});
+it("Should transform a string number to a number of type number", async () => {
+  //Arrange
+  const numbers = ["1", "3"];
+  const expectedResult = 4;
+  //Act
+  const result = Sum(numbers);
+  //Assert
+  expect(result).toBe(expectedResult);
 });
