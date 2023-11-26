94d6c2f9ee48252ab1cdfa7e5bb103c941d79424
Mehradi <mehradi.info@gmail.com>
2023-11-26 18:17:27 +0100

    Adding test: 'generateReportData(): should execute logFn if provided'

--- src/data.test.ts
+++ src/data.test.ts
@@ -1,4 +1,10 @@
-import { it, describe } from "vitest";
+import { it, describe, vi, expect } from "vitest";
 import { generateReportData } from "./data";
 
-describe();
+describe("generateReportData()", () => {
+  it.only("should execute logFn if provided", () => {
+    const logger = vi.fn();
+    generateReportData(logger);
+    expect(logger).toBeCalled();
+  });
+});
