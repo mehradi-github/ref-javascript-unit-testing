4b9f8365de937edb17df59168dc06088bacc2c92
Mehradi <mehradi.info@gmail.com>
2023-11-26 18:31:55 +0100

    Adding test: 'should execute the writeFile function'

--- src/data.test.ts
+++ src/data.test.ts
@@ -2,7 +2,7 @@ import { it, describe, vi, expect } from "vitest";
 import { generateReportData } from "./data";
 
 describe("generateReportData()", () => {
-  it.only("should execute logFn if provided", () => {
+  it("should execute logFn if provided", () => {
     const logger = vi.fn();
     generateReportData(logger);
     expect(logger).toBeCalled();
