682eb80c6cb012659df53dbc4b53ed75eb7f33ce
Mehradi <mehradi.info@gmail.com>
2023-11-28 20:43:29 +0100

    Adding test:'should return a promise that resolves to no value if called correctly'

--- src/io.ts
+++ src/io.ts
@@ -1,7 +1,9 @@
 import path from "path";
 import { promises as fs } from "fs";
 
-export const writeData = (data: string, fileName: string) => {
+const writeData = (data: string, fileName: string) => {
   const storagePath = path.join(process.cwd(), "data", fileName);
   return fs.writeFile(storagePath, data);
 };
+
+export default writeData;
