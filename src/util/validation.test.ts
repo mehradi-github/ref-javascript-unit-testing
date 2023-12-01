2bc1353199311d50c79630d1e804c85e8c749cfc
Mehradi <mehradi.info@gmail.com>
2023-12-01 16:26:23 +0100

    should throw an error if an empty string is provided as a value

--- src/util/validation.test.ts
+++ src/util/validation.test.ts
@@ -9,3 +9,10 @@ it("should throw an error if an empty string is provided as a value", () => {
 
   expect(validationFn).toThrow();
 });
+it("should throw an error if an empty string is provided as a value", () => {
+  const testInput = "  ";
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).toThrow();
+});
